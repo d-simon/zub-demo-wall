c65c15b4990618e1687ffe4592518c178f8f6bc9
David Simon <me@davidsimon.ch>
2015-02-18 00:02:08 +0100

    [Feature] Themes: Add option to define and switch between themes

--- wall-generator.js
+++ wall-generator.js
@@ -8,33 +8,52 @@
     };
 
 
-    var elementsAmount = 200;
-
+    /** Defaults */
+    var elementsAmount = 200,
+        currentTheme = zub.wallThemes['default'];
 
     /**
      * Generates a random wall of images and patterns.
      * @param {dom-element} Wall element to "wallify".
      * @param {function} A template function.
      */
-    function initWall (el, template) {
+    function initWall (el, template, theme) {
         var output = '<div>';
 
         for (var i = 0; i < elementsAmount; i++) {
-            output += template();
+            output += template(theme);
         }
 
         output += '</div>'
-        $(el).append(output);
+        $(el).empty().append(output);
     }
 
-
+    /**
+     * Ready Event / Setup
+     */
     $(document).ready(function () {
+
         var $template = $('#poster-template'),
             templateStr = $template.html() || 'No Template found!',
-            template = _.template(templateStr);
+            template = _.template(templateStr),
+            $wall = $('[data-js-wall]');
 
-        $('[data-js-wall]').each(function (index, el) {
-            initWall(el, template);
+        /** Generate inital wall */
+        initWall($wall, template, currentTheme);
+
+        /** Generate theme navigation */
+        _.forEach(zub.wallThemes, function (theme, name) {
+            $('[data-js-theme-nav]')
+                .append('<button type="button" class="theme-nav-button" data-js-wall-theme="'+name+'">'+name+'</button>');
+        });
+
+        /** Bind theme navigation */
+        $('[data-js-wall-theme]').click(function (e) {
+            var themeName = $(this).attr('data-js-wall-theme');
+            if (themeName && zub.wallThemes[themeName]) {
+                currentTheme = zub.wallThemes[themeName];
+                initWall($wall, template, currentTheme);
+            }
         });
 
     });
